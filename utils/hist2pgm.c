1d2af5ed4f785ed06f4377bf3e2169b8944b1085
Amithash Prasad <amithash@gmail.com>
2010-11-21 21:49:26 -0700

    modified hist2pgm to fit the new method

--- utils/hist2pgm.c
+++ utils/hist2pgm.c
@@ -5,27 +5,38 @@
 #include "hist.h"
 
 int main(int argc, char *argv[]) {
-	hist_t hist;
-	spect_t spect;
+	hist_t *hist = NULL;
+	hist_t *hist_list = NULL;
 	int rc;
 	FILE *pgf;
 	char outf_name[256] = "./tmp.pgm";
 	char buf[100];
 	int i,j;
-	double max_val[NBANDS];
+	unsigned int len;
 
-	if(argc < 2) {
-		printf("USAGE: %s FILE.spect4 (Optional output pgm file default: tmp.pgm)\n", argv[0]);
+	if(argc < 3) {
+		printf("USAGE: %s <spectdb> FILE.spect4 (Optional output pgm file default: tmp.pgm)\n", argv[0]);
 		exit(-1);
 	}
-	if((rc = read_spectf(argv[1], &spect)) != 0) {
+	if((rc = read_hist_db(&hist_list, &len, argv[1])) != 0) {
 		printf("Reading spect file %s failed with error=%s\n",argv[1], RM_RC_STR(rc));
 		exit(-1);
 	}
-	spect2hist(&hist, &spect);
 
-	if(argc > 2) {
-		strcpy(outf_name, argv[2]);
+	for(i = 0; i < len; i++) {
+		if(strcmp(hist_list[i].fname, argv[2]) == 0) {
+			printf("Found at index %d\n", i);
+			hist = &hist_list[i];
+			break;
+		}
+	}
+	if(!hist) {
+		spect_error("Could not find %s in db", argv[2]);
+		goto cleanup;
+	}
+
+	if(argc == 4) {
+		strcpy(outf_name, argv[3]);
 	}
 	pgf = fopen(outf_name, "w");
 	if(pgf == NULL) {
@@ -35,14 +46,8 @@ int main(int argc, char *argv[]) {
 	sprintf(buf, "P5\n%d %d\n255\n", SPECT_HIST_LEN, NBANDS);
 	write(fileno(pgf), buf, strlen(buf) * sizeof(char));
 	for(i = 0; i < NBANDS; i++) {
-		max_val[i] = 0;
 		for(j = 0; j < SPECT_HIST_LEN; j++) {
-			if(hist.spect_hist[i][j] > max_val[i]) {
-				max_val[i] = hist.spect_hist[i][j];
-			}
-		}
-		for(j = 0; j < SPECT_HIST_LEN; j++) {
-			unsigned int ui_val = (unsigned int)(hist.spect_hist[i][j] * 255);
+			unsigned int ui_val = (unsigned int)(hist->spect_hist[i][j] * 255);
 			unsigned char val = (unsigned char)(ui_val & 0xff);
 			if(write(fileno(pgf), &val, sizeof(unsigned char)) != sizeof(unsigned char)) {
 				printf("Write error!\n");
@@ -50,7 +55,8 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	fclose(pgf);
-	free_spect(&spect);
+cleanup:
+	free(hist_list);
 
 	return 0;
 }
