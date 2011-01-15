0cf5b51ec6df7807384ee7ea75e29fb72906690f
Amithash Prasad <amithash@gmail.com>
2011-01-15 08:29:29 -0700

    added pgm file

--- lib/plot.c
+++ lib/plot.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
-
+#include <float.h>
+#include <string.h>
 #include "plot.h"
 
 #define error(fmt) fprintf(stderr, fmt)
@@ -58,7 +60,56 @@ int plot(float *x, float *y, unsigned int ncol, unsigned int len, plot_type_t ty
 	if(!debug) {
 		system("rm -f __out.gp __data.txt");
 	}
+	return 0;
 }
+
+int pgm(char *fname, float *_data, unsigned int len_x, unsigned int len_y)
+{
+	float min = FLT_MAX, max = 0;
+	int i,j;
+	char buf[256];
+	FILE *f;
+	float *data;
+	int rc = -1;
+
+	if(!_data)
+	      return -1;
+	f = fopen(fname, "w");
+	if(!f)
+	      return -2;
+
+	data = (float *)malloc(len_x * len_y * sizeof(float));
+	if(!data)
+	      return -3;
+	memcpy(data, _data, len_x * len_y * sizeof(float));
+
+	for(i = 0; i < len_x * len_y; i++) {
+		if(data[i] < min)
+		      min = data[i];
+		if(data[i] > max)
+		      max = data[i];
+	}
+	for(i = 0; i < len_x * len_y; i++) {
+		data[i] = 255 * (data[i] - min) / (max - min);
+	}
+	sprintf(buf, "P5\n%d %d\n255\n", len_x, len_y);
+	if(write(fileno(f), buf, strlen(buf) * sizeof(char)) != sizeof(char))
+	      goto cleanup;
+	for(i = 0; i < len_y; i++) {
+		for(j = 0; j < len_x; j++) {
+			unsigned int ui_val = (unsigned int)data[(i * len_x) + j];
+			unsigned char val = 255 - (unsigned char)(ui_val & 0xff);
+			if(write(fileno(f), &val, sizeof(unsigned char)) != sizeof(unsigned char))
+			      goto cleanup;
+		}
+	}
+	rc = 0;
+
+cleanup:
+	free(data);
+	return rc;
+}
+
 #ifdef PLOT_TEST
 int main(void)
 {
